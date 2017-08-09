21d251ab91d15c37452e5f1d924d4b9035ab52a2
rabell66 <raabell66@gmail.com>
2017-08-09 13:23:13 -0500

    Initial commit

---
+++ src/main/java/com/ironmovies/theIronMovies/TheIronMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.ironmovies.theIronMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheIronMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheIronMoviesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ironmovies/theIronMovies/TheIronMoviesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ironmovies.theIronMovies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheIronMoviesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
